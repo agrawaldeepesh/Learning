b94ddf8695ff6be3dbc9cde14952b3969cd57eff
Deepesh Agrawal <deepesh.agrawal@wales.nhs.uk>
2025-03-08 20:31:37 +0000

    Type Script learning

---
+++ Playwright_Typescript/variable_dataType.ts
@@ -0,0 +1,28 @@
+//console.log("Hello Deepesh")
+
+let firstName:String = "Deepesh";
+let lastName = 'Agrawal';
+let age:any = 20;
+
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+
+age=22;
+console.log (age);
+
+firstName = 'd'
+age = false
+
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+
+let emptyVariable:undefined;
+
+console.log (emptyVariable);
+console.log (typeof(emptyVariable))
+
+let nullValue:null = null;
+console.log (nullValue);
+console.log (typeof(nullValue))
